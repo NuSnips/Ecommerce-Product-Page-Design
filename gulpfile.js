e845f7e5bad56730c2a8ffee3b008ca6aea167f5
nuwanthi.desilva <105438670+Nuwanthi-DeSilva@users.noreply.github.com>
2022-08-21 13:23:52 +0530

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const prefix = require("gulp-autoprefixer");
+const cssnano = require("cssnano");
+const terser = require("gulp-terser");
+const browserSync = require("browser-sync").create();
+
+//Sass task
+function scssTask() {
+    return src("app/scss/**/*.scss", { sourcemaps: true })
+        .pipe(sass())
+        .on("error", sass.logError)
+        .pipe(prefix("last 2 versions"))
+        .pipe(postcss([cssnano()]))
+        .pipe(dest("dist", { sourcemaps: "." }));
+}
+
+//Javascript task
+function jsTask() {
+    return src("app/js/*.js", { sourcemaps: true })
+        .pipe(terser())
+        .pipe(dest("dist", { sourcemaps: "." }));
+}
+
+//BrowserSync Task
+function browserSyncServe(cb) {
+    browserSync.init({
+        server: {
+            baseDir: ".",
+        },
+    });
+    cb();
+}
+
+//Watch Task
+function watchTask() {
+    watch("*.html", browserSyncReload);
+    watch(
+        ["app/scss/**/*.scss", "app/js/**/*.js"],
+        series(scssTask, jsTask, browserSyncReload)
+    );
+}
+
+function browserSyncReload(cb) {
+    browserSync.reload();
+    cb();
+}
+
+//Default gulp task
+exports.default = series(
+    scssTask,
+    jsTask,
+    browserSyncServe,
+    browserSyncReload,
+    watchTask
+);
